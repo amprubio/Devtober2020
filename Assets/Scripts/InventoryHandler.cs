8ce1f0afc9526866039d4eda024a3785d1d22332
Amph <amprubio@ucm.es>
2020-10-17 19:58:20 +0200

    movido inventario a otro script para evitar conflictos  + carpeta fonts creada

---
+++ Assets/Scripts/InventoryHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryHandler : MonoBehaviour
+{
+    Canvas invState;
+
+    void Start()
+    {
+        if (GameObject.Find("Inventory_UI"))
+            invState = GameObject.Find("Inventory_UI").GetComponent<Canvas>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory") && invState)
+        {
+            Inventory();
+        }
+    }
+
+    public bool isRunning()
+    {
+        return invState;
+    }
+
+    public void Inventory()
+    {
+        if (invState.enabled == false)
+        {
+            invState.enabled = true;
+        }
+        else if (invState.enabled)
+        {
+            invState.enabled = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,14 +16,11 @@ namespace Yarn.Unity.Example
 
         NavMeshAgent agent;
 
-        Canvas invState;
-
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
 
-            if (GameObject.Find("Inventory_UI"))
-                invState = GameObject.Find("Inventory_UI").GetComponent<Canvas>();
+           
 
             cam = Camera.main;
         }
@@ -43,16 +40,13 @@ namespace Yarn.Unity.Example
             //Click y Posicionamiento
             if (Physics.Raycast(cam.transform.position, ray.direction, out hit, 100))
             {
-                if (Input.GetButtonDown("Fire1") && invState.enabled == false)
+                if (Input.GetButtonDown("Fire1") && FindObjectOfType<InventoryHandler>().isRunning())
                     SetPosition();
 
                 Debug.DrawRay(cam.transform.position, ray.direction * hit.distance);
             }
 
-            if (Input.GetButtonDown("Inventory") && invState)
-            {
-                Inventory();
-            }
+           
         }
 
         void SetPosition()
@@ -60,17 +54,7 @@ namespace Yarn.Unity.Example
             agent.destination = hit.point;
         }
 
-        void Inventory()
-        {
-            if (invState.enabled == false)
-            {
-                invState.enabled = true;
-            }
-            else if (invState.enabled)
-            {
-                invState.enabled = false;
-            }
-        }
+     
 
         private void OnDrawGizmos()
         {
